5c4dae486377cc552d2c0d48bc616b52cdf881f2
Marek Roslik <marek.roslik@gmail.com>
2021-12-13 12:18:40 +0300

    Initial Commit

---
+++ WeatherAPI/WeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIApp.swift
+//  WeatherAPI
+//
+//  Created by Marek Roslik on 13.12.21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
