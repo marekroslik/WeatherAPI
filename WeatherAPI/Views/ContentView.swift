d50d073c9c37f5afa79f9ac7c75125eadb09b762
Marek Roslik <marek.roslik@gmail.com>
2021-12-22 23:52:22 +0300

    0.2

--- WeatherAPI/Views/ContentView.swift
+++ WeatherAPI/Views/ContentView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
     
     var body: some View {
         VStack {
             
             if let location = locationManager.location{
-                Text("Your coordinates are:  \(location.longitude),  \(location.latitude)")
+                if let weather = weather {
+                    Text("Data error")
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                               weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print ("Error \(error)")
+                            }
+                        }
+                }
             } else {
                 if locationManager.isLoading {
                     LoadingView()
